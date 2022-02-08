bc0900a4599fbfd0bca89fd24789ea8396b7f9c2
sandip0721 <rathod2sandip1997@gmail.com>
2022-02-08 18:33:09 +0530

    changes

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,42 +1,42 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
-import './Row.css';
+import "./Row.css";
 
+const base_url = "https://image.tmdb.org/t/p/original/";
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
 
+  // A snippet of code which runs based on a sppecific condition/variable
+  useEffect(() => {
+    //ifm[], run once when the row loads, and dont run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      console.log(request.data.results);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-const base_url = "https://image.tmdb.org/t/p/original/"
-function Row({title, fetchUrl}){
-    const [movies, setMovies] = useState([]);
+  console.log(movies);
 
-    // A snippet of code which runs based on a sppecific condition/variable
-    useEffect (()=>{
-        //ifm[], run once when the row loads, and dont run again
-        async function fetchData(){
-            const request = await axios.get(fetchUrl);
-            console.log(request.data.results);
-            setMovies(request.data.results)
-            return request;
-        }
-        fetchData();
-    },[fetchUrl]);
-
-    console.log(movies);
-
-    return(
-        <div className="row">
-            {/* title */}
-            <h2>{title}</h2>
-            <div className="row_posters">
-                {/* row_poster */}
-                {movies.map(movie => (
-                    <img
-                    className="row_poster"
-                     src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
-                 ) )}
-            </div>
-            {/* container -> posters */}
-        </div>
-    )
+  return (
+    <div className="row">
+      {/* title */}
+      <h2>{title}</h2>
+      <div className="row_posters">
+        {/* row_poster */}
+        {movies.map((movie) => (
+          <img
+            className="row_poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {/* container -> posters */}
+    </div>
+  );
 }
 
 export default Row;
