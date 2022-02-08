c21097157f03a3bce01baeeae00a501e120521b0
sandip0721 <rathod2sandip1997@gmail.com>
2022-02-08 18:20:50 +0530

    netflix-clone

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import requests from './request';
+import Row from './Row';
+function App() {
+  return (
+    <div className="App">
+      Hey prgrammer
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      
+    </div>
+  );
+}
+
+export default App;
